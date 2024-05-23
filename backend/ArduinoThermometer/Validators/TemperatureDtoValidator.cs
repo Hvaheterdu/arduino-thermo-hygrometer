99b3e2e5d305e02b4106b13aea1e43ac8ef70888
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-05-23 20:51:22 +0200

    Refactor projects

--- backend/ArduinoThermometer/Validators/TemperatureDtoValidator.cs
+++ backend/ArduinoThermometer/Validators/TemperatureDtoValidator.cs
@@ -10,6 +10,6 @@ public class TemperatureDtoValidator : AbstractValidator<Temperature>
         RuleFor(t => t.TemperatureGUID).NotEmpty().NotNull();
         RuleFor(t => t.TemperatureReadingDate).NotEmpty().NotNull().LessThanOrEqualTo(DateTimeOffset.Now.Date);
         RuleFor(t => t.TemperatureReadingTime).NotEmpty().NotNull().LessThanOrEqualTo(DateTimeOffset.Now);
-        RuleFor(t => t.TemperatureReading).NotEmpty().NotNull();
+        RuleFor(t => t.TemperatureValue).NotEmpty().NotNull();
     }
 }
