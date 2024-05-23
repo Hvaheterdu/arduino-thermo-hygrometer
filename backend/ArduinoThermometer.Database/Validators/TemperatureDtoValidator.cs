f554857155d5f90a3ae8278e3f5f024a39e6dfe1
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-05-23 19:44:42 +0200

    Refactor class name

---
+++ backend/ArduinoThermometer.Database/Validators/TemperatureDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using ArduinoThermometer.Data.Models;
+using FluentValidation;
+
+namespace ArduinoThermometer.Data.Validators;
+
+public class TemperatureDtoValidator : AbstractValidator<Temperature>
+{
+    public TemperatureDtoValidator()
+    {
+        RuleFor(t => t.TemperatureGUID).NotEmpty().NotNull();
+        RuleFor(t => t.TemperatureReadingDate).NotEmpty().NotNull().LessThanOrEqualTo(DateTimeOffset.Now.Date);
+        RuleFor(t => t.TemperatureReadingTime).NotEmpty().NotNull().LessThanOrEqualTo(DateTimeOffset.Now);
+        RuleFor(t => t.TemperatureReading).NotEmpty().NotNull();
+    }
+}
