0675152f550c3437562b233856219e0ddef4555e
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-05-27 20:11:14 +0200

    Testing pre commit hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
 export const App = (): JSX.Element => {
-  console.log("HELLO");
-  console.log("HELLO");
-  console.log("HELLO");
-  console.log("HELLO");
-  console.log("HELLO");
   return (
     <>
       <h1>React 18 Alpha</h1>
