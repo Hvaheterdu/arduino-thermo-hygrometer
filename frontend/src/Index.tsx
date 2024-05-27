f75f7adf06c1323b4778d5dd6775353c5baf15d5
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-05-27 21:28:07 +0200

    Test

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 
 const rootElement = document.getElementById("root") ?? document.createElement("div");
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <StrictMode>
