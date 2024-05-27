b7212a7bab4f5cb9f1c7911a988d804fa16110f6
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-05-27 20:01:22 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ export const App = (): JSX.Element => {
   console.log("HELLO");
   console.log("HELLO");
   console.log("HELLO");
+  console.log("HELLO");
   return (
     <>
       <h1>React 18 Alpha</h1>
