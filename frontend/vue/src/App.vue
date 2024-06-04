1023057e6b2b4d24f4c64bf17f1c547bb0f0330d
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-06-04 23:04:38 +0200

    Bump version from 0.0.0 to 0.0.1

---
+++ frontend/vue/src/App.vue
@@ -0,0 +1,8 @@
+<!-- eslint @typescript-eslint/naming-convention: 0 -->
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Vue 3 Alpha</h1>
+</template>
+
+<style scoped></style>
---
+++ frontend/vue/src/Index.ts
@@ -0,0 +1,7 @@
+/* eslint filename-rules/match: 0 */
+/* eslint @typescript-eslint/naming-convention: 0 */
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.mount("#app");
