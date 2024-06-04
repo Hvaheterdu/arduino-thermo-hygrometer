c8427dc6f238da33adfb38da738bee88ecd50334
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-06-04 20:29:35 +0200

    Bump version from 0.0.47 to 0.0.48

--- frontend/react/src/vite-env.d.ts
+++ frontend/react/src/vite-env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_ENVIRONMENT: string;
-  readonly VITE_BUILDID: string;
   readonly VITE_APIBASEURL: string;
-  readonly VITE_APIAUTHCONFIG: string;
-  readonly VITE_CLIENTID: string;
 }
 
 interface ImportMeta {
