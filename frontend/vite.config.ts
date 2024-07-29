23c7d7bf55c64cb69df10e88771574dd59662aa6
Burhan Mohammad Sarfraz <141680612+Burhan-Mohammad-Sarfraz@users.noreply.github.com>
2024-07-29 18:39:47 +0200

    Cleanup. Remember to fix issue with script-src unsafe-inline (#99)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,9 +18,12 @@ export default defineConfig({
     strictPort: true,
     cors: true,
     headers: {
+      "content-security-policy":
+        "base-uri 'self'; default-src 'self'; frame-ancestors 'none'; manifest-src 'none'; script-src 'unsafe-inline' 'self';",
+      "permissions-policy":
+        "accelerometer=(), autoplay=(), bluetooth=(), camera=(), compute-pressure=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), hid=(), identity-credentials-get=(), idle-detection=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-create=(), publickey-credentials-get=(), screen-wake-lock=(), storage-access=(), sync-xhr=(), usb=(), web-share=(), window-management=(), xr-spatial-tracking=()",
       "referrer-policy": "no-referrer",
       "strict-transport-security": "max-age=31536000; includeSubDomains; preload",
-      "x-frame-options": "SAMEORIGIN",
       "x-content-type-options": "nosniff",
     },
   },
