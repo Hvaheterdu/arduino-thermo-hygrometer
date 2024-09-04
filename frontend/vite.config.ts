fb50bfbf70f7471ecc2c282be49db9c83466a041
Burhan Mohammad Sarfraz <burhan.sarfraz@bouvet.no>
2024-09-04 08:26:52 +0200

    Fix CSP header and fix no-rename-default error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
 /* eslint import-x/no-default-export: 0 */
-import react from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
-import svgrPlugin from "vite-plugin-svgr";
+import plugin_default from "vite-plugin-mkcert";
+import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [mkcert(), react(), svgrPlugin()],
+  plugins: [plugin_default(), viteReact(), vitePluginSvgr()],
   build: {
     outDir: "build",
     sourcemap: true,
@@ -21,7 +21,7 @@ export default defineConfig({
     },
     headers: {
       "content-security-policy":
-        "base-uri 'self'; default-src 'self'; frame-ancestors 'none'; manifest-src 'none'; script-src 'self';",
+        "base-uri 'self'; default-src 'self'; frame-ancestors 'none'; manifest-src 'none'; script-src 'unsafe-inline' 'self';",
       "permissions-policy":
         "accelerometer=(), autoplay=(), bluetooth=(), camera=(), compute-pressure=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), hid=(), identity-credentials-get=(), idle-detection=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-create=(), publickey-credentials-get=(), screen-wake-lock=(), storage-access=(), sync-xhr=(), usb=(), web-share=(), window-management=(), xr-spatial-tracking=()",
       "referrer-policy": "no-referrer",
