a1cfea0303c414e178a01a965cc33eb8362918fb
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2024-10-03 20:55:24 +0200

    Add checker and fix build dist folder

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,16 +2,31 @@
 
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { checker } from "vite-plugin-checker";
 import plugin_default from "vite-plugin-mkcert";
 import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin_default(), viteReact(), vitePluginSvgr()],
+  plugins: [
+    checker({
+      overlay: {
+        initialIsOpen: true,
+        position: "br",
+      },
+      typescript: true,
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "src/**" --max-warnings 0',
+      },
+    }),
+    plugin_default(),
+    viteReact(),
+    vitePluginSvgr(),
+  ],
   build: {
     outDir: "./dist",
     reportCompressedSize: true,
-    sourcemap: true,
     minify: true,
   },
   server: {
@@ -23,7 +38,7 @@ export default defineConfig({
     },
     headers: {
       "content-security-policy":
-        "base-uri 'self'; default-src 'self'; frame-ancestors 'none'; manifest-src 'none'; script-src 'unsafe-inline' 'self';",
+        "base-uri 'self'; default-src 'self'; frame-ancestors 'none'; manifest-src 'none'; script-src 'self'; style-src 'self'",
       "permissions-policy":
         "accelerometer=(), autoplay=(), bluetooth=(), camera=(), compute-pressure=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), hid=(), identity-credentials-get=(), idle-detection=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-create=(), publickey-credentials-get=(), screen-wake-lock=(), storage-access=(), sync-xhr=(), usb=(), web-share=(), window-management=(), xr-spatial-tracking=()",
       "referrer-policy": "no-referrer",
