671c97842f66ae0af02f342630af97dacfe1422b
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2026-06-01 16:10:51 +0200

    Cleanup.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,6 @@ import { checker } from "vite-plugin-checker";
 import plugin from "vite-plugin-mkcert";
 import vitePluginSvgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root: import.meta.dirname,
   plugins: [
@@ -47,11 +46,11 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     cors: {
-      origin: "https://localhost:5000"
+      origin: "http://localhost:5000"
     },
     headers: {
       "content-security-policy":
-        "base-uri 'self'; default-src 'self'; frame-ancestors 'none'; manifest-src 'none'; script-src 'unsafe-inline' 'self'; style-src 'unsafe-inline' 'self'",
+        "base-uri 'self'; connect-src 'self' https://localhost:5000; default-src 'self'; frame-ancestors 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; worker-src 'self'",
       "permissions-policy":
         "accelerometer=(), autoplay=(), bluetooth=(), camera=(), compute-pressure=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), hid=(), identity-credentials-get=(), idle-detection=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-create=(), publickey-credentials-get=(), screen-wake-lock=(), storage-access=(), sync-xhr=(), usb=(), web-share=(), window-management=(), xr-spatial-tracking=()",
       "referrer-policy": "no-referrer",
