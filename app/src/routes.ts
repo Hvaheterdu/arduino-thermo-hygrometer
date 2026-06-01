7194f66031244c59db2b4a08a559daeaaa60cf8a
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2026-06-01 16:13:23 +0200

    Cleanup.

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,31 +1,11 @@
-import { createBrowserRouter, redirect, type DataRouter, type RouteObject } from "react-router";
-import { About } from "./pages/about/About.page";
-import { Home } from "./pages/home/Home.page";
-import { Statistics } from "./pages/statistics/Statistics.page";
+import { createBrowserRouter, type DataRouter, type RouteObject } from "react-router";
 
 const basename = "/arduinothermohygrometer";
 
 const routes: RouteObject[] = [
   {
     path: "/",
-    children: [
-      {
-        index: true,
-        loader: () => redirect("/home")
-      },
-      {
-        path: "home",
-        Component: Home
-      },
-      {
-        path: "about",
-        Component: About
-      },
-      {
-        path: "statistics",
-        Component: Statistics
-      }
-    ]
+    children: []
   }
 ];
 
