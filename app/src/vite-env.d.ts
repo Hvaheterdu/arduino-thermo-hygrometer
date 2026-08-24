187e426fdcc3eea873e0b7dae77296d7867fdba4
Burhan Sarfraz <burhan.mohammad.sarfraz@outlook.com>
2026-08-25 01:26:55 +0200

    Refactors frontend.

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,13 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly MODE: string;
-  readonly VITE_BASE_PATH?: string;
-  readonly VITE_API_BASEURL?: string;
-  readonly VITE_API_BASEURL_LOCAL?: string;
-  readonly VITE_API_BASEURL_DEVELOPMENT?: string;
-  readonly VITE_API_BASEURL_STAGING?: string;
-  readonly VITE_API_BASEURL_PRODUCTION?: string;
+  readonly VITE_BASE_PATH: string;
+  readonly VITE_API_BASEURL_LOCAL: string;
+  readonly VITE_API_BASEURL_DEVELOPMENT: string;
+  readonly VITE_API_BASEURL_STAGING: string;
+  readonly VITE_API_BASEURL_PRODUCTION: string;
   readonly VITE_API_HEADER_NAME?: string;
   readonly VITE_API_KEY?: string;
 }
